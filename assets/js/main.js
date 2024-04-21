58bf0c06613347148b750168a4c3bf561d6f3147
Harini Srinivasan <harinisree333@gmail.com>
2024-04-22 00:51:10 +0530

    The project filter is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,11 +40,42 @@ function scrollHeader()
 }
 window.addEventListener("scroll" , scrollHeader)
 
-/*=============== TESTIMONIAL SWIPER ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== PORTFOLIO ITEM FILTER ===============*/
+const filterContainer = document.querySelector(".project-filter-inner"),
+    filterBtns = filterContainer.children,
+    totalFilterBtn = filterBtns.length,
+    projectItems = document.querySelectorAll(".project-item"),
+    totalProjectItem = projectItems.length;
+
+    for(let i=0;i<totalFilterBtn;i++){
+        filterBtns[i].addEventListener("click",function()
+        {
+            filterContainer.querySelector(".active").classList.remove("active");
+            this.classList.add("active");
+
+            const filterValue = this.getAttribute("data-filter");
+            for(let k =0;k<totalProjectItem;k++){
+                if(filterValue === projectItems[k].getAttribute("data-category"))
+                {
+                    projectItems[k].classList.remove("hide");
+                    projectItems[k].classList.add("show");
+                }else{
+                    projectItems[k].classList.add("hide");
+                    projectItems[k].classList.remove("show");
+                }
+
+                if(filterValue === "all")
+                {
+                    projectItems[k].classList.remove("hide");
+                    projectItems[k].classList.add("show");
+                }
+            }
+
+        })
+    }
 
 /*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
 const theme = document.querySelector("#theme-button");
